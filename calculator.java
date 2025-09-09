735a1a5ced21dc12a51e3dc47f972f90a8de27f0
Amrutha18032007 <24wh1a05w0@bvrithyderabad.edu.in>
2025-09-09 10:52:01 +0530

    calculator.java

--- calculator.java
+++ calculator.java
@@ -6,14 +6,12 @@ public class calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input two numbers
         System.out.print("Enter first number: ");
         double num1 = scanner.nextDouble();
 
         System.out.print("Enter second number: ");
         double num2 = scanner.nextDouble();
 
-        // Input operator
         System.out.print("Enter operator (+, -, *, /): ");
         char operator = scanner.next().charAt(0);
 
@@ -52,3 +50,4 @@ public class calculator {
         scanner.close();
     }
 }
+
